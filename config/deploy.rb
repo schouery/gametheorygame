f6f585ba14efda0f1571e385fb569e0335600505
Rafael Crivellari Saliba Schouery <schouery@gmail.com>
2010-04-29 16:43:32 -0300

    More Capistrano, but not done yet.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,32 +5,22 @@ set :user, 'schouery'
 
 set :scm, :git
 set :deploy_to, "/Users/schouery/Desktop/deploy"
-# set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "127.0.0.1"                          # Your HTTP server, Apache/etc
-role :app, "127.0.0.1"                          # This may be the same as your `Web` server
-role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
+role :web, "127.0.0.1"
+role :app, "127.0.0.1"
+role :db,  "127.0.0.1", :primary => true
+
 set :use_sudo, false
 set :scm_command, '/opt/local/bin/git'
 set :local_scm_command, :default
 
-
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
   task :symlink_shared do
-    system "scp config/database.yml #{user}@#{server}:#{shared_path}/"
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    files = ['database.yml', 'facebooker.yml']
+    files.each do |f|
+      system "scp config/#{f} #{user}@#{server}:#{shared_path}/"
+      run "ln -nfs #{shared_path}/#{f} #{release_path}/config/#{f}"
+    end
   end
 end
 
