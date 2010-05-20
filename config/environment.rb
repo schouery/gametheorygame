e1cca59f9f104681123bec64a142ed041367340a
Rafael Crivellari Saliba Schouery <schouery@gmail.com>
2010-05-20 13:08:04 -0300

    Missing alteration of environment for Rake task card_dealer

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+require File.join(File.dirname(__FILE__), '../lib/card_dealer')
+
 Rails::Initializer.run do |config|
   # Forgery Protection não funciona bem no Facebook
   config.action_controller.allow_forgery_protection = false
